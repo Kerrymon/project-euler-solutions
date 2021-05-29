077b6ff9c7d95d6e2a3677edbbfed54202b9cf5e
Kerrymon <krollwtf@gmail.com>
2021-05-29 08:06:10 +0500

    Minor changes
    add 24, 25 and small changes in other ones

--- 18.py
+++ 18.py
@@ -1,4 +1,5 @@
-"""By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+"""Euler Problem #18:
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
 
 3
 7 4
